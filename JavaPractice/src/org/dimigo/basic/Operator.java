1213d384159ee3434eef3c4ffc6c8303856df4d2
KAMAPNA <KAMAPNA@KAMAPNA-LAPTOP>
2017-03-13 10:30:39 +0900

    실습과제 2 디미베네 제출

--- JavaPractice/src/org/dimigo/basic/Operator.java
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -25,10 +25,9 @@ public class Operator {
 		long money = 1700000;
 		long people = 3;
 		long store = 1500;
-		
 		long yearmoney = money * 12 * people * store;
-		
 		System.out.println("<<디미베네 연간 인건비>>");
+		
 		System.out.println("월 평균 급여 : "+String.format("%,d", money)+"원");
 		System.out.println("점포 내 직원 수 : "+people+"명");
 		System.out.println("점포 수 : "+String.format("%,d", store)+"개");
