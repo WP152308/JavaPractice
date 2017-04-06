b1f324c955f26e908b477cd04eb7bc2723c96aea
KAMAPNA <KAMAPNA@KAMAPNA-LAPTOP>
2017-04-06 11:22:45 +0900

    실습6 제출

---
+++ JavaPractice/src/org/dimigo/oop/Snack.java
@@ -0,0 +1,95 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *  |_ Snack
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 4. 5.
+ * </pre>
+ *
+ * @author : KAMAPNA
+ * @version : 1.0
+ *
+ */
+public class Snack {
+	private String name;
+	private String company;
+	private int price;
+	private int number;
+	
+	public Snack(){
+		
+	}
+	public Snack(String name, String company, int price, int number){
+		this.company = company;
+		this.name = name;
+		this.price = price;
+		this.number = number;
+	}
+	
+	
+	public int calcPrice(){
+		return this.price*this.number; 
+	}
+	
+	public String toString(){
+		return "이름 : " + name +"\n"+"제조사 : " + company +"\n"+"가격 : " + String.format("%,d", price) +"\n개수 : " + number+"\n";
+	}
+	
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the company
+	 */
+	public String getCompany() {
+		return company;
+	}
+	/**
+	 * @param company the company to set
+	 */
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	/**
+	 * @return the price
+	 */
+	public int getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	/**
+	 * @return the number
+	 */
+	public int getNumber() {
+		return number;
+	}
+	/**
+	 * @param number the number to set
+	 */
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
+}
---
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+import java.text.Format;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *  |_ SnackTest
+ *
+ * 1. 개요 : 실습과제6 과자배열
+ * 2. 작성일 : 2017. 4. 5.
+ * </pre>
+ *
+ * @author : KAMAPNA
+ * @version : 1.0
+ *
+ */
+public class SnackTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int sum = 0;
+		Snack[] snack = new Snack[]{
+				new Snack("세우깡", "농심", 1100, 2),
+				new Snack("콘칲", "크라운", 1200, 1),
+				new Snack("허니버터칩", "해태", 1500, 4),
+		};
+
+		for (int i = 0; i < snack.length; i++) {
+			sum += snack[i].calcPrice();
+			System.out.println(snack[i].toString());
+		}
+		
+		System.out.println("총 구매 금액 : "+String.format("%,d", sum));
+		
+		
+		
+		
+	}
+
+}
