75a6dee1d5caaeb398be2986170f225875ecc6c2
KAMAPNA <KAMAPNA@KAMAPNA-LAPTOP>
2017-06-15 11:37:34 +0900

    과제 17제출

---
+++ JavaPractice/src/org/dimigo/io/ArakoMenu.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package org.dimigo.io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.Writer;
+
+/**
+ * <pre>
+ * org.dimigo.io
+ *  |_ ArakoMenu
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 6. 15.
+ * </pre>
+ *
+ * @author : KAMAPNA
+ * @version : 1.0
+ *
+ */
+public class ArakoMenu {
+
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				Writer writer = new BufferedWriter(new FileWriter("files/menu.txt"));
+				BufferedReader reader = new BufferedReader(new FileReader("files/menu.txt"))) {
+			String data;
+			while((data = br.readLine()) != null){
+				writer.write(data+"\n");
+			}
+			int result;
+			char[] buf = new char[1024];
+			while((result = reader.read(buf)) != -1){
+				writer.write(buf, 0, result);
+			}
+			writer.flush();
+			System.out.println("<< 메뉴 출력 >>");
+			String str;
+			while ((str = reader.readLine()) != null) {
+				System.out.println(str);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
